8b7f0ae4b10704fac9927aa49c9fa2a6eb2e4d3d
gcamargocamposrocha <gcamargocamposrocha@guidewire.com>
2022-01-01 16:41:17 +0000

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const dbConnect = require("./config/db/dbConnect");
+
+dotenv.config();
 
 const app = express();
+dbConnect();
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
